67173df896b57654bea6553abdd2072b64716077
Thomas Saint-Gérand <devspaceship@gmail.com>
2018-12-20 11:43:01 +0100

    implemented different cells for gridworld and created grid customization system

---
+++ scripts/grid.js
@@ -0,0 +1,61 @@
+const AIR = 0;
+const START = 1;
+const END = 2;
+const TRAP = 3;
+
+class Grid
+{
+    constructor(n=8, m=12)
+    {
+        this.n = n;
+        this.m = m;
+
+        this.start_i = 0;
+        this.start_j = 0;
+
+        this.end_i = n - 1;
+        this.end_j = m - 1;
+
+        if (n == 8 && m == 12)
+        {
+            this.map = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0],
+                        [0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0],
+                        [0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0],
+                        [0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2]];
+        }
+        else
+        {
+            // Initialize the map
+            this.map = new Array(n);
+            for (let i = 0; i < n; i++)
+            {
+                this.map[i] = new Array(m);
+                this.map[i].fill(AIR);
+            }
+            this.map[this.start_i][this.start_j] = START;
+            this.map[this.end_i][this.end_j] = END;
+        }
+    }
+
+    draw()
+    {
+        noStroke();
+
+        for (let i = 0; i < this.n; i++) 
+        {
+            for (let j = 0; j < this.m; j++) 
+            {
+                if (this.map[i][j] == AIR) { fill(0, 0, 0, 0); }
+                else if (this.map[i][j] == START) { fill('#f49d41'); }
+                else if (this.map[i][j] == END) { fill('#42f465'); }
+                else { fill('#f44141'); }
+
+                rect(j*(CELL_SIZE+3)+3, i*(CELL_SIZE+3)+3, CELL_SIZE, CELL_SIZE);
+            }
+        }
+    }
+}
--- scripts/gridworld.js
+++ scripts/gridworld.js
@@ -1,18 +1,53 @@
 const N = 8;
 const M = 12;
 const CELL_SIZE = 50;
+const GRID = new Grid(N, M);
 
 function setup() 
 {
     let cnv = createCanvas(M*CELL_SIZE + (M+1)*3, N*CELL_SIZE + (N+1)*3);
     cnv.parent('canvas-holder');
-    noLoop();
 }
   
 function draw() 
 {
     background(30);
+    GRID.draw();
     drawGrid();
+
+    noLoop();
+}
+
+function mousePressed()
+{
+    mouseDragged();
+}
+
+function mouseDragged()
+{
+    let i = Math.floor(mouseY/(CELL_SIZE+3));
+    let j = Math.floor(mouseX/(CELL_SIZE+3));
+    let current = GRID.map[i][j];
+
+    let block_type = select('#block-type').value();
+    if (block_type == 'air' && current == TRAP) { GRID.map[i][j] = AIR; }
+    else if (block_type == 'trap' && current == AIR) { GRID.map[i][j] = TRAP; }
+    else if (block_type == 'start' && (current == AIR || current == TRAP))
+    {
+        GRID.map[GRID.start_i][GRID.start_j] = AIR;
+        GRID.start_i = i;
+        GRID.start_j = j;
+        GRID.map[i][j] = START;
+    }
+    else if (block_type == 'end' && (current == AIR || current == TRAP))
+    {
+        GRID.map[GRID.end_i][GRID.end_j] = AIR;
+        GRID.end_i = i;
+        GRID.end_j = j;
+        GRID.map[i][j] = END;
+    }
+
+    loop();
 }
 
 function drawGrid()
@@ -29,5 +64,5 @@ function drawGrid()
     {
         line(j*(CELL_SIZE+3)+1, 0, j*(CELL_SIZE+3)+1, height-1);
     }
-
 }
+
