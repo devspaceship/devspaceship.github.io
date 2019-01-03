b945c7d9589dfd2929b4eee00ea80e6783c1814b
Thomas Saint-Gérand <devspaceship@gmail.com>
2019-01-03 17:58:11 +0100

    created some dom elements and focused mouse handling onto canvas

--- scripts/gridworld.js
+++ scripts/gridworld.js
@@ -7,6 +7,7 @@ function setup()
 {
     let cnv = createCanvas(M*CELL_SIZE + (M+1)*3, N*CELL_SIZE + (N+1)*3);
     cnv.parent('canvas-holder');
+    cnv.mouseMoved(canvasClicked);
 }
   
 function draw() 
@@ -15,39 +16,39 @@ function draw()
     GRID.draw();
     drawGrid();
 
+    updateDom();
+
     noLoop();
 }
 
-function mousePressed()
+function canvasClicked()
 {
-    let i = Math.floor(mouseY/(CELL_SIZE+3));
-    let j = Math.floor(mouseX/(CELL_SIZE+3));
-    let current = GRID.map[i][j];
-
-    let block_type = select('#block-type').value();
-    if (block_type == 'air' && current == TRAP) { GRID.map[i][j] = AIR; }
-    else if (block_type == 'trap' && current == AIR) { GRID.map[i][j] = TRAP; }
-    else if (block_type == 'start' && (current == AIR || current == TRAP))
+    if (mouseIsPressed)
     {
-        GRID.map[GRID.start_i][GRID.start_j] = AIR;
-        GRID.start_i = i;
-        GRID.start_j = j;
-        GRID.map[i][j] = START;
-    }
-    else if (block_type == 'end' && (current == AIR || current == TRAP))
-    {
-        GRID.map[GRID.end_i][GRID.end_j] = AIR;
-        GRID.end_i = i;
-        GRID.end_j = j;
-        GRID.map[i][j] = END;
-    }
+        let i = Math.floor(mouseY/(CELL_SIZE+3));
+        let j = Math.floor(mouseX/(CELL_SIZE+3));
+        let current = GRID.map[i][j];
 
-    loop();
-}
+        let block_type = select('#block-type').value();
+        if (block_type == 'air' && current == TRAP) { GRID.map[i][j] = AIR; }
+        else if (block_type == 'trap' && current == AIR) { GRID.map[i][j] = TRAP; }
+        else if (block_type == 'start' && (current == AIR || current == TRAP))
+        {
+            GRID.map[GRID.start_i][GRID.start_j] = AIR;
+            GRID.start_i = i;
+            GRID.start_j = j;
+            GRID.map[i][j] = START;
+        }
+        else if (block_type == 'end' && (current == AIR || current == TRAP))
+        {
+            GRID.map[GRID.end_i][GRID.end_j] = AIR;
+            GRID.end_i = i;
+            GRID.end_j = j;
+            GRID.map[i][j] = END;
+        }
 
-function mouseDragged()
-{
-    mousePressed();
+        loop();
+    }
 }
 
 function drawGrid()
@@ -63,6 +64,15 @@ function drawGrid()
     for (let j = 0; j <= M; j++) 
     {
         line(j*(CELL_SIZE+3)+1, 0, j*(CELL_SIZE+3)+1, height-1);
+    }
+}
+
+function updateDom()
+{
+    let solver = select('#solver-type').value();
+    if (solver == "policy_iter")
+    {
+
     }
 }
 
