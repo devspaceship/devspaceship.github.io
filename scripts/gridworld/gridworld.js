36af57d2d0890d768d0e27cc35cf73fac55f6966
Thomas Saint-Gérand <devspaceship@gmail.com>
2019-01-05 17:16:45 +0100

    linked sliders labels to js

--- scripts/gridworld/gridworld.js
+++ scripts/gridworld/gridworld.js
@@ -10,6 +10,11 @@ let policy_iter_treshold, policy_iter_gamma;
 let value_iter_treshold, value_iter_gamma, value_iter_k;
 let SARSA_Q_treshold, SARSA_Q_gamma, SARSA_Q_alpha, SARSA_Q_eps_0, SARSA_Q_T;
 
+let policy_iter_treshold_label, policy_iter_gamma_label;
+let value_iter_treshold_label, value_iter_gamma_label, value_iter_k_label;
+let SARSA_Q_treshold_label, SARSA_Q_gamma_label, SARSA_Q_alpha_label;
+let SARSA_Q_eps_0_label, SARSA_Q_T_label;
+
 function setup() 
 {
     let cnv = createCanvas(M*CELL_SIZE + (M+1)*3, N*CELL_SIZE + (N+1)*3);
@@ -23,6 +28,7 @@ function setup()
     value_iter_options = select('#value_iter_options');
     SARSA_Q_options = select('#SARSA_Q_options');
 
+    // Link sliders
     policy_iter_treshold = select('#policy_iter_treshold');
     policy_iter_gamma = select('#policy_iter_gamma');
 
@@ -36,7 +42,32 @@ function setup()
     SARSA_Q_eps_0 = select('#SARSA_Q_eps_0');
     SARSA_Q_T = select('#SARSA_Q_T');
 
-    
+    // Link sliders labels
+    policy_iter_treshold_label = select('#policy_iter_treshold_value');
+    policy_iter_gamma_label = select('#policy_iter_gamma_value');
+
+    value_iter_treshold_label = select('#value_iter_treshold_value');
+    value_iter_gamma_label = select('#value_iter_gamma_value');
+    value_iter_k_label = select('#value_iter_k_value');
+
+    SARSA_Q_treshold_label = select('#SARSA_Q_treshold_value');
+    SARSA_Q_gamma_label = select('#SARSA_Q_gamma_value');
+    SARSA_Q_alpha_label = select('SARSA_Q_alpha_value');
+    SARSA_Q_eps_0_label = select('#SARSA_Q_eps_0_value');
+    SARSA_Q_T_label = select('#SARSA_Q_T_value');
+
+    policy_iter_treshold.changed(updateSlidersValues);
+    policy_iter_gamma.changed(updateSlidersValues);
+
+    value_iter_treshold.changed(updateSlidersValues);
+    value_iter_gamma.changed(updateSlidersValues);
+    value_iter_k.changed(updateSlidersValues);
+
+    SARSA_Q_treshold.changed(updateSlidersValues);
+    SARSA_Q_gamma.changed(updateSlidersValues);
+    SARSA_Q_alpha.changed(updateSlidersValues);
+    SARSA_Q_eps_0.changed(updateSlidersValues);
+    SARSA_Q_T.changed(updateSlidersValues);
 
     solver_type_select.changed(updateDom);
     updateDom();
@@ -129,4 +160,9 @@ function updateDom()
         value_iter_options.hide();
         SARSA_Q_options.show();
     }
+}
+
+function updateSlidersValues()
+{
+
 }
